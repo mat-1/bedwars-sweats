19fb787615d83c89c113310ecbc761632fe5c24a
mat <github@matdoes,dev>
2020-07-17 23:53:48 -0500

    ping command

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'Logged in as {client}')
 
-@bot.command(name='list')
-async def list_command(ctx, arg):
-    pass
+@bot.command(name='ping')
+async def ping_command(ctx):
+    await ctx.send('Pong!')
 
 with open('token.txt', 'r') as f:
 	token = f.read().strip()
